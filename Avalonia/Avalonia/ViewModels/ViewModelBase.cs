8c8468d95ad5ace9d708227d59a37b5aa7b74ec2
Roger Briggen <roger@rogerbriggen.ch>
2026-06-26 07:14:16 +0200

    Add empty Avalonia app

---
+++ Avalonia/Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Avalonia/Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,14 @@
+
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Avalonia.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
